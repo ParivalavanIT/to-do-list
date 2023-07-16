10a96415d2ca8c7415103cd5f9ab938a4cfad13f
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2023-07-16 07:22:22 +0530

    minor changes

--- index.js
+++ index.js
@@ -128,8 +128,8 @@ app.post("/delete", async (req, res) => {
   }
 });
 
+const port = 3000;
 
-
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server started on port 3000");
 });
